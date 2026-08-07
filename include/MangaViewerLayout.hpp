285fcb262825176a03fedb9d8953111009a94e42
David Abejón <davidabejonheras@gmail.com>
2026-08-07 10:53:37 +0200

    add. zoom and 2D panning to manga viewer
    Refactors page positioning from a single scroll offset to independent X/Y scrolling so all view modes can pan both axes. Adds right-stick zoom with clamped min/max scale, introduces target-size based resizing, and reapplies the current sizing mode when loading pages so zoom level is preserved instead of resetting.

--- include/MangaViewerLayout.hpp
+++ include/MangaViewerLayout.hpp
@@ -24,12 +24,18 @@ class MangaViewerLayout : public pu::ui::Layout {
         };
 
         static constexpr s32 ScrollSpeed = 25;
+        static constexpr s32 ZoomSpeed = 10;
+        static constexpr double MinZoomFraction = 0.3;
+        static constexpr double MaxZoomFraction = 3.0;
 
         void LoadPage(const u32 index);
         void ApplyViewMode();
+        void ApplyCurrentMode();
         void ApplyWidthMode(const s32 width);
         void ApplyHeightMode(const s32 height);
-        void SetScrollOffset(const s32 offset);
+        void ApplyDimensions(const s32 width, const s32 height);
+        void AdjustZoom(const s32 delta);
+        void SetScroll(const s32 x, const s32 y);
 
         std::string manga_path;
         std::vector<std::string> page_files;
@@ -37,9 +43,13 @@ class MangaViewerLayout : public pu::ui::Layout {
         ViewMode mode;
         s32 tex_width;
         s32 tex_height;
-        s32 scroll_offset;
-        s32 max_scroll_offset;
-        s32 center_offset;
+        s32 target_size;
+        s32 scroll_x;
+        s32 scroll_y;
+        s32 max_scroll_x;
+        s32 max_scroll_y;
+        s32 center_offset_x;
+        s32 center_offset_y;
         OnBack on_back;
         pu::ui::elm::Image::Ref pageImage;
         pu::ui::elm::TextBlock::Ref pageIndicator;
--- source/MangaViewerLayout.cpp
+++ source/MangaViewerLayout.cpp
@@ -1,7 +1,7 @@
 #include <MangaViewerLayout.hpp>
 #include <FsUtils.hpp>
 
-MangaViewerLayout::MangaViewerLayout(const std::string &manga_path) : Layout::Layout(), manga_path(manga_path), page_files(fs::ListImageFiles(manga_path)), current_page(0), mode(ViewMode::Vertical), tex_width(0), tex_height(0), scroll_offset(0), max_scroll_offset(0), center_offset(0) {
+MangaViewerLayout::MangaViewerLayout(const std::string &manga_path) : Layout::Layout(), manga_path(manga_path), page_files(fs::ListImageFiles(manga_path)), current_page(0), mode(ViewMode::Vertical), tex_width(0), tex_height(0), target_size(0), scroll_x(0), scroll_y(0), max_scroll_x(0), max_scroll_y(0), center_offset_x(0), center_offset_y(0) {
     this->SetBackgroundColor(pu::ui::Color(0, 0, 0, 0xFF));
 
     this->pageIndicator = pu::ui::elm::TextBlock::New(1700, 20, "");
@@ -46,21 +46,25 @@ MangaViewerLayout::MangaViewerLayout(const std::string &manga_path) : Layout::La
             }
         }
 
-        if (this->mode == ViewMode::Horizontal) {
-            if (keys_held & HidNpadButton_StickLLeft) {
-                this->SetScrollOffset(this->scroll_offset - MangaViewerLayout::ScrollSpeed);
-            }
-            else if (keys_held & HidNpadButton_StickLRight) {
-                this->SetScrollOffset(this->scroll_offset + MangaViewerLayout::ScrollSpeed);
-            }
+        if (keys_held & HidNpadButton_StickLLeft) {
+            this->SetScroll(this->scroll_x - MangaViewerLayout::ScrollSpeed, this->scroll_y);
         }
-        else {
-            if (keys_held & HidNpadButton_StickLUp) {
-                this->SetScrollOffset(this->scroll_offset - MangaViewerLayout::ScrollSpeed);
-            }
-            else if (keys_held & HidNpadButton_StickLDown) {
-                this->SetScrollOffset(this->scroll_offset + MangaViewerLayout::ScrollSpeed);
-            }
+        else if (keys_held & HidNpadButton_StickLRight) {
+            this->SetScroll(this->scroll_x + MangaViewerLayout::ScrollSpeed, this->scroll_y);
+        }
+
+        if (keys_held & HidNpadButton_StickLUp) {
+            this->SetScroll(this->scroll_x, this->scroll_y - MangaViewerLayout::ScrollSpeed);
+        }
+        else if (keys_held & HidNpadButton_StickLDown) {
+            this->SetScroll(this->scroll_x, this->scroll_y + MangaViewerLayout::ScrollSpeed);
+        }
+
+        if (keys_held & HidNpadButton_StickRUp) {
+            this->AdjustZoom(MangaViewerLayout::ZoomSpeed);
+        }
+        else if (keys_held & HidNpadButton_StickRDown) {
+            this->AdjustZoom(-MangaViewerLayout::ZoomSpeed);
         }
     });
 }
@@ -89,7 +93,12 @@ void MangaViewerLayout::LoadPage(const u32 index) {
     }
 
     this->current_page = index;
-    this->ApplyViewMode();
+    if (this->target_size <= 0) {
+        this->ApplyViewMode();
+    }
+    else {
+        this->ApplyCurrentMode();
+    }
     this->pageIndicator->SetText(std::to_string(index + 1) + " / " + std::to_string(this->page_files.size()));
 }
 
@@ -100,76 +109,125 @@ void MangaViewerLayout::ApplyViewMode() {
 
     switch (this->mode) {
         case ViewMode::Vertical: {
-            this->ApplyWidthMode(static_cast<s32>(pu::ui::render::ScreenWidth));
+            this->target_size = static_cast<s32>(pu::ui::render::ScreenWidth);
             break;
         }
         case ViewMode::Intermediate: {
             const auto full_width = static_cast<s32>(pu::ui::render::ScreenWidth);
             const auto fit_height_width = static_cast<s32>((static_cast<double>(this->tex_width) * pu::ui::render::ScreenHeight) / this->tex_height);
-            this->ApplyWidthMode((full_width + fit_height_width) / 2);
+            this->target_size = (full_width + fit_height_width) / 2;
             break;
         }
         case ViewMode::Horizontal: {
-            this->ApplyHeightMode(static_cast<s32>(pu::ui::render::ScreenHeight));
+            this->target_size = static_cast<s32>(pu::ui::render::ScreenHeight);
             break;
         }
     }
 
-    this->SetScrollOffset(0);
+    this->ApplyCurrentMode();
 }
 
-void MangaViewerLayout::ApplyWidthMode(const s32 width) {
-    const auto height = static_cast<s32>((static_cast<double>(this->tex_height) * width) / this->tex_width);
-
-    this->pageImage->SetWidth(width);
-    this->pageImage->SetHeight(height);
-    this->pageImage->SetX((static_cast<s32>(pu::ui::render::ScreenWidth) - width) / 2);
-
-    this->max_scroll_offset = height - static_cast<s32>(pu::ui::render::ScreenHeight);
-    if (this->max_scroll_offset < 0) {
-        this->center_offset = (static_cast<s32>(pu::ui::render::ScreenHeight) - height) / 2;
-        this->max_scroll_offset = 0;
-    }
-    else {
-        this->center_offset = 0;
-    }
-}
-
-void MangaViewerLayout::ApplyHeightMode(const s32 height) {
-    const auto width = static_cast<s32>((static_cast<double>(this->tex_width) * height) / this->tex_height);
-
-    this->pageImage->SetWidth(width);
-    this->pageImage->SetHeight(height);
-    this->pageImage->SetY((static_cast<s32>(pu::ui::render::ScreenHeight) - height) / 2);
-
-    this->max_scroll_offset = width - static_cast<s32>(pu::ui::render::ScreenWidth);
-    if (this->max_scroll_offset < 0) {
-        this->center_offset = (static_cast<s32>(pu::ui::render::ScreenWidth) - width) / 2;
-        this->max_scroll_offset = 0;
-    }
-    else {
-        this->center_offset = 0;
-    }
-}
-
-void MangaViewerLayout::SetScrollOffset(const s32 offset) {
-    auto clamped = offset;
-    if (clamped < 0) {
-        clamped = 0;
-    }
-    else if (clamped > this->max_scroll_offset) {
-        clamped = this->max_scroll_offset;
-    }
-
-    this->scroll_offset = clamped;
-    if (this->pageImage == nullptr) {
+void MangaViewerLayout::ApplyCurrentMode() {
+    if ((this->pageImage == nullptr) || (this->tex_width <= 0) || (this->tex_height <= 0)) {
         return;
     }
 
     if (this->mode == ViewMode::Horizontal) {
-        this->pageImage->SetX(this->center_offset - this->scroll_offset);
+        this->ApplyHeightMode(this->target_size);
     }
     else {
-        this->pageImage->SetY(this->center_offset - this->scroll_offset);
+        this->ApplyWidthMode(this->target_size);
     }
+
+    this->SetScroll(0, 0);
+}
+
+void MangaViewerLayout::ApplyWidthMode(const s32 width) {
+    const auto height = static_cast<s32>((static_cast<double>(this->tex_height) * width) / this->tex_width);
+    this->ApplyDimensions(width, height);
+}
+
+void MangaViewerLayout::ApplyHeightMode(const s32 height) {
+    const auto width = static_cast<s32>((static_cast<double>(this->tex_width) * height) / this->tex_height);
+    this->ApplyDimensions(width, height);
+}
+
+void MangaViewerLayout::ApplyDimensions(const s32 width, const s32 height) {
+    this->pageImage->SetWidth(width);
+    this->pageImage->SetHeight(height);
+
+    this->max_scroll_x = width - static_cast<s32>(pu::ui::render::ScreenWidth);
+    if (this->max_scroll_x < 0) {
+        this->center_offset_x = (static_cast<s32>(pu::ui::render::ScreenWidth) - width) / 2;
+        this->max_scroll_x = 0;
+    }
+    else {
+        this->center_offset_x = 0;
+    }
+
+    this->max_scroll_y = height - static_cast<s32>(pu::ui::render::ScreenHeight);
+    if (this->max_scroll_y < 0) {
+        this->center_offset_y = (static_cast<s32>(pu::ui::render::ScreenHeight) - height) / 2;
+        this->max_scroll_y = 0;
+    }
+    else {
+        this->center_offset_y = 0;
+    }
+}
+
+void MangaViewerLayout::AdjustZoom(const s32 delta) {
+    if ((this->pageImage == nullptr) || (this->tex_width <= 0) || (this->tex_height <= 0)) {
+        return;
+    }
+
+    const auto is_height_based = (this->mode == ViewMode::Horizontal);
+    const auto screen_size = is_height_based ? static_cast<s32>(pu::ui::render::ScreenHeight) : static_cast<s32>(pu::ui::render::ScreenWidth);
+
+    const auto min_size = static_cast<s32>(screen_size * MangaViewerLayout::MinZoomFraction);
+    const auto max_size = static_cast<s32>(screen_size * MangaViewerLayout::MaxZoomFraction);
+
+    auto size = this->target_size + delta;
+    if (size < min_size) {
+        size = min_size;
+    }
+    else if (size > max_size) {
+        size = max_size;
+    }
+
+    this->target_size = size;
+    if (is_height_based) {
+        this->ApplyHeightMode(size);
+    }
+    else {
+        this->ApplyWidthMode(size);
+    }
+
+    this->SetScroll(this->scroll_x, this->scroll_y);
+}
+
+void MangaViewerLayout::SetScroll(const s32 x, const s32 y) {
+    auto clamped_x = x;
+    if (clamped_x < 0) {
+        clamped_x = 0;
+    }
+    else if (clamped_x > this->max_scroll_x) {
+        clamped_x = this->max_scroll_x;
+    }
+
+    auto clamped_y = y;
+    if (clamped_y < 0) {
+        clamped_y = 0;
+    }
+    else if (clamped_y > this->max_scroll_y) {
+        clamped_y = this->max_scroll_y;
+    }
+
+    this->scroll_x = clamped_x;
+    this->scroll_y = clamped_y;
+    if (this->pageImage == nullptr) {
+        return;
+    }
+
+    this->pageImage->SetX(this->center_offset_x - this->scroll_x);
+    this->pageImage->SetY(this->center_offset_y - this->scroll_y);
 }
